10691eced2140914deacada279e527448e9ddf59
CHAFAY98 <chafay091@gmail.com>
2020-01-11 14:05:19 +0100

    new language sidebar

---
+++ resources/lang/en/giftTeams.php
@@ -0,0 +1,10 @@
+<?php
+return[
+    'staff_management'=>'Staff management',
+    'add_new_employee'=>'إAdd a new employee',
+    'name'=>'Name',
+    'email'=>'Email',
+    'adress'=>'Address',
+    'password'=>'Password ',
+    'employee_powers'=>'Employee powers'
+];
