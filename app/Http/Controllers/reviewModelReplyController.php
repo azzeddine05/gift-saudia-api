c0c47d975e8ea95d3d988b672791701bc6f41fde
CHAFAY98 <chafay091@gmail.com>
2020-02-20 22:31:02 +0100

    review reply v1

--- app/Http/Controllers/reviewModelReplyController.php
+++ app/Http/Controllers/reviewModelReplyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\MainStandard;
 use App\TotalMark;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class reviewModelReplyController extends Controller
 {
@@ -19,11 +20,19 @@ class reviewModelReplyController extends Controller
         $readyModelFields =  MainStandard::all();
         $totalMarks = TotalMark::first();
         $total=0;
+        $compaby_id=1;
         if (isset($totalMarks->total))
             $total=$totalMarks->total;
-        $restOftotal = $total/*fhad total fin kayn erro*/ - $totalWeightMainStandard;
+        $restOftotal = $total/*fhad total fin kayn error*/ - $totalWeightMainStandard;
+        //get sub standars
+        $reviews= DB::table('sub_standards')
+            ->select('review_items.arabic_name','review_items.id', 'sub_standards.arabic_name as standard_title', 'ready_model_replies.value','ready_model_replies.company_id')
+            ->join('association', 'sub_standards.id', '=', 'association.id_sub_standar')
+            ->join('ready_model_replies', 'ready_model_replies.sub_standard_id', '=', 'association.id_sub_standar')
+            ->join('review_items', 'review_items.id', '=', 'association.id_review')->where('ready_model_replies.company_id','=',$compaby_id)->orderBy('sub_standards.arabic_name')->get();
+        echo "<script>console.log(".json_encode( $reviews).");</script>";
         return view('reviews.readyModelReply', ['readyModelFields' => $readyModelFields, 'totalMarks' => $totalMarks,
-            'totalWeightMainStandard' => $totalWeightMainStandard, 'restOftotal' => $restOftotal
+            'totalWeightMainStandard' => $totalWeightMainStandard, 'restOftotal' => $restOftotal,'reviews'=>$reviews
         ]);
     }
 
--- resources/views/reviews/readyModelReply.blade.php
+++ resources/views/reviews/readyModelReply.blade.php
@@ -32,67 +32,86 @@
     <!-- ============================================================== -->
     <div class="content-page  @lang('sidebar.right_class')">
         <!-- Start content -->
+
         <div class="content">
             <div class="container-fluid">
-                <div class="row">
                     <div id="akbarDiv" class="col-md-12">
                         <div class="card-box">
-                            <h4 class="m-t-0 m-b-30 header-title">اسم معيار فرعي </h4>
-                            <div class="m-b-15">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <form id="mainStandard" class="form-group">
-                                            <div class="form-group mb-2">
-                                                    <p style="margin-left: 10px" class="">ما تقول المنشأة على هذا المعيار </p>
-                                            </div>
 
-                                        </form>
-                                    </div>
+                            <?php $duplicat="";$fix=$reviews[0]->value; ?>
+
+                            @foreach($reviews as $review)
+                                @if($review->value!=$fix)
+                                        <div class="col text-center my-3">
+                                            <button type="button" class="btn btn-success" >حفظ</button>
+                                        </div>
                                 </div>
-                            </div>
-                            <div class="m-b-15">
-                                    <button id="show-standar-container" type="" class="btn btn-primary addMainStandard ">أضف نقطة قوة أو فرصة تحسين </button>
-                            </div>
-
-                            <div id="standar-container" style="display : none;">
-                                <form id="standars-form" method="POST">
-                                    <div id="all_power">
-                                        <div class="row">
-                                            <div class="col-md-11 form-group">
-                                                <label  for="inputPawor1" class="mx-2">نقطة قوة&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : </label>
-                                                <input id="inputPawor1" class="form-control" />
+                                @endif
+                                @if($duplicat!=$review->standard_title)
+                                    <div class="my-5 container-form">
+                                        <h4 class="m-t-0 m-b-30 header-title">{{$review->standard_title}}</h4>
+                                        <div class="form-group m-b-15">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <form id="mainStandard" class="form-group">
+                                                        <div class="form-group mb-2">
+                                                            <textarea class="sub-standar-value form-control" class="form-control" rows="5" value="">{{$review->value}}</textarea>
+                                                        </div>
+                                                    </form>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <button type="button" class="btn btn-primary my-3" id="add-power">
-                                        <i class="fa fa-plus"></i>
-                                    </button>
+                                        <div class="m-b-15">
+                                            <button id="show-standar-container" type="" class="btn btn-primary addMainStandard ">أضف نقطة قوة أو فرصة تحسين </button>
+                                        </div>
 
-                                    <div id="all_improve">
-                                        <div class="row">
-                                            <div class="col-md-11 form-group ">
-                                                <label  for="improve1" class="mx-2">فرصة تحسين : </label>
-                                                <textarea id="improve1" class="form-control"></textarea>
-                                            </div>
+                                        <div id="standar-container" style="display : none;">
+                                            <form id="standars-form" method="POST">
+
+                                                <div id="all_power">
+                                                    <div class="row">
+                                                        <div class="col-md-11 form-group">
+                                                            <label  for="inputPawor1" class="mx-2">نقطة قوة&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : </label>
+                                                            <input id="inputPawor1" class="form-control" />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <button type="button" class="btn btn-primary my-3" id="add-power">
+                                                    <i class="fa fa-plus"></i>
+                                                </button>
+
+                                                <div id="all_improve">
+                                                    <div class="row">
+                                                        <div class="col-md-11 form-group ">
+                                                            <label  for="improve1" class="mx-2">فرصة تحسين : </label>
+                                                            <textarea id="improve1" class="form-control"></textarea>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <button type="button" class="btn btn-primary my-3" id="add-improve" >
+                                                    <i class="fa fa-plus"></i>
+                                                </button>
+
+                                                <div class="form-action text-center">
+                                                    <button type="submit" class="btn btn-success">حفظ الآن </button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        @endif
+                                        <div class="row d-flex mb-2">
+                                            <label for="" class="mx-5"> {{$review->arabic_name}} :</label>
+                                            <input type="text" name="" id="" class="form-control w-75" placeholder="" aria-describedby="helpId">
+                                        </div>
+                                <?php $duplicat=$review->standard_title; ?>
+
+                                <?php $fix= $review->value; ?>
+                                @endforeach
+                                        <div class="col text-center my-3">
+                                            <button type="button" class="btn btn-success" >حفظ</button>
                                         </div>
                                     </div>
-                                    <button type="button" class="btn btn-primary my-3" id="add-improve" >
-                                        <i class="fa fa-plus"></i>
-                                    </button>
-
-                                    <div class="form-action text-center">
-                                        <button type="submit" class="btn btn-success">حفظ الآن </button>
-                                    </div>
-                                </form>
                             </div>
-
-                        </div>
                     </div>
-                </div>
-
-
-
-
         </div> <!-- container -->
 
     </div> <!-- content -->
